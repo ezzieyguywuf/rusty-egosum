e0bcc5c6024d6fede28fbc535a0e65430613de61
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-09-22 13:41:11 -0400

    Update test to expect output wrapped in quotes
    Tests fail
    
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -73,7 +73,7 @@ mod tests {
     #[test]
     fn parse_valid_string() {
         let parsed = parse_gosum_line("github.com/dlclark/regexp2 v1.4.0/go.mod h1:2pZnwuY/m+8K6iRw6wQdMtk+rH5tNGR1i55kozfMjCc=");
-        let check  = "github.com/dlclark/regexp2 v1.4.0/go.mod";
+        let check  = "\"github.com/dlclark/regexp2 v1.4.0/go.mod\"";
 
         assert_eq!(parsed.unwrap(), check);
     }
