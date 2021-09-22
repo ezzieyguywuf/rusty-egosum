b518bb3daa3cc11db37f776a08f6f3de78522f79
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-09-22 09:37:00 -0400

    Add first failing test
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn parse_gosum_line(_: &str) -> &str {
+    return "";
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_gosum_line_works() {
+        let parsed = parse_gosum_line("github.com/dlclark/regexp2 v1.4.0/go.mod h1:2pZnwuY/m+8K6iRw6wQdMtk+rH5tNGR1i55kozfMjCc=");
+        let check  = "github.com/dlclark/regexp2 v1.4.0/go.mod";
+
+        assert_eq!(parsed, check);
+    }
 }
